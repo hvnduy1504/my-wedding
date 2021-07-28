e4c16fcca5a8f4c6fc67319ce3d7dd40b9911c6a
Duy Huynh <hvnduy1504@gmail.com>
2021-07-28 13:47:21 +0700

    Update template and integrate backend with Rest API

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const http = require('http');
+const path = require('path');
+// import the book recommendations module
+const myWedding = require('./service/MyWedding.js');
 
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+// serve your css as static
+// so we can load all the html,css and javacript files from the public folder
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+
+	var options = {
+	  host: 'localhost',
+	  port: 8080,
+	  path: '/guests/',
+	  method: 'GET'
+	};
+	var body = [];
+	http.request(options, function(restRes) {
+		// console.log('STATUS: ' + restRes.statusCode);
+		// console.log('HEADERS: ' + JSON.stringify(restRes.headers));
+		restRes.setEncoding('utf8');
+		restRes.on('data', function (chunk) {
+			body = chunk; 
+		});
+
+		// This is called when the request is finished and response is returned
+		// Hence, use the constructed body string to parse it to json and return
+		restRes.on('end', function () {
+		   //console.log('whole response > ' + body); 
+		 	var jsonObject = JSON.parse(body);
+			jsonObject.content.forEach(e => {
+				if (e.name) {
+					e.avatar = myWedding.convertTextToImage(myWedding.getFirstLetterOfWord(e.name));
+				}
+			})
+			res.render('index', {data:jsonObject.content}); 
+		});
+	}).end();
+	
+    //res.render('index', {data: body});
 });
 
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Application started and Listening on port: ${PORT}`));
---
+++ service/MyWedding.js
@@ -0,0 +1,18 @@
+const textToImage = require('text-to-image');
+
+var exports=module.exports={};
+exports.getFirstLetterOfWord=function(str) {
+	var matches = str.match(/\b(\w)/g);
+	return matches.join('');
+};
+
+exports.convertTextToImage=function(str) {
+	return textToImage.generateSync(str, {
+					  maxWidth: 100,
+					  fontSize: 50,
+					  fontFamily: 'Arial',
+					  bgColor: "rgba(241, 78, 149, 1)",
+					  textColor: "#ffffff",
+					  textAlign: "center",
+					});
+};
