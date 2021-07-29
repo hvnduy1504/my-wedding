883704155fc2e9c6999424bfb35f560b3938bc6d
Duy Huynh <hvnduy1504@gmail.com>
2021-07-29 11:23:24 +0700

    Implement Friend Attending

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const http = require('http');
 const path = require('path');
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 // import the book recommendations module
 const myWedding = require('./service/MyWedding.js');
 
@@ -13,12 +21,11 @@ app.use(express.static(path.join(__dirname, "public")));
 
 
 app.get('/', (req, res) => {
-
-	var options = {
-	  host: 'my-wedding-backend.herokuapp.com',
-	  //port: 8080,
-	  path: '/guests/',
-	  method: 'GET'
+		var options = {
+		host: myWedding.apiHost,
+		// port: 8080,
+		path: '/guests/',
+		method: 'GET'
 	};
 	var body = [];
 	http.request(options, function(restRes) {
@@ -32,7 +39,7 @@ app.get('/', (req, res) => {
 		// This is called when the request is finished and response is returned
 		// Hence, use the constructed body string to parse it to json and return
 		restRes.on('end', function () {
-		   console.log('whole response > ' + body); 
+		   	// console.log('whole response > ' + body); 
 		 	var jsonObject = JSON.parse(body);
 			jsonObject.content.forEach(e => {
 				if (e.name) {
@@ -42,8 +49,49 @@ app.get('/', (req, res) => {
 			res.render('index', {data:jsonObject.content}); 
 		});
 	}).end();
-	
-    //res.render('index', {data: body});
 });
+
+app.post('/guest', (request, response) => {
+	if (!request.body.isPublishMessage) {
+		request.body.isPublishMessage = false;
+	}
+	const postData = JSON.stringify(request.body);
+	console.log(`REQUEST BODY: ${postData}`);
+	
+	var resBody = [];
+	var options = {
+		host: myWedding.apiHost,
+	  //port: 8080,
+		path: '/guest',
+		method: 'POST',
+		headers: {
+		    'Content-Type': 'application/json',
+		    'Content-Length': Buffer.byteLength(postData)
+		}
+    };
+	var req = http.request(options, (res) => {
+	console.log(`STATUS: ${res.statusCode}`);
+	console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+	res.setEncoding('utf8');
+	res.on('data', (chunk) => {
+		// console.log(`BODY: ${chunk}`);
+		// body = JSON.parse(chunk)
+		resBody = chunk;
+		// console.log(`BODY: ${chunk}`);
+	});
+	res.on('end', () => {
+		console.log(`RESPONSE: ${resBody}`);
+		response.send(resBody);
+		});
+	});
+
+	req.on('error', (e) => {
+		console.error(`problem with request: ${e.message}`);
+	});
+
+	// Write data to request body
+	req.write(postData);
+	req.end();
+});  
 
 app.listen(PORT, () => console.log(`Application started and Listening on port: ${PORT}`));
--- service/MyWedding.js
+++ service/MyWedding.js
@@ -1,6 +1,10 @@
 const textToImage = require('text-to-image');
 
+
 var exports=module.exports={};
+
+exports.apiHost='my-wedding-backend.herokuapp.com';
+
 exports.getFirstLetterOfWord=function(str) {
 	var matches = str.match(/\b(\w)/g);
 	return matches.join('');
